0aa30a6058a95db58ea41380450d590acfb0f224
AkashYadav-02 <akashydv2212@gmail.com>
2021-08-16 14:40:18 +0530

    intial commit

---
+++ nav.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Grand  Bank</title>
+  <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="css/index.css">
+  <link rel="stylesheet" type="text/css" href="css/table.css">
+  <link rel="stylesheet" type="text/css" href="css/createuser.css">
+  <link rel="stylesheet" type="text/css" href="css/contact.css">
+  <script type="text/javascript" src="bootstrap/js/bootstrap.bundle.min.js"></script>
+  <link rel = "icon" href = "img/logo.png" type = "image/x-icon">
+</head>
+
+
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color:#67DEA0;">
+    <div class="container-fluid">
+    <img src="img\bank-building.png" width="50" height="60">
+      <a class="navbar-brand logo" href="index.php" style="font-weight: bold; font-size: 25px;">Grand Bank</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+
+
+        <ul class="navbar-nav">
+          <li class="nav-item btn btn-secondary">
+            <a class="nav-link text-white" href="index.php"style="border-radius: 20%;background-color: #57ADe6; /* Green */
+  
+  color: white;
+  
+  
+  font-size: 16px;
+  margin: 4px 1px;
+  cursor: pointer; ">Home</a>
+          </li>
+         
+          <li class="nav-item btn btn-secondary" >
+            <a class="nav-link link-text text-white" href="contactme.php"style="border-radius: 10%;background-color: #57ADD6; /* Green */
+  border: none;
+  color: white;
+  
+  
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;">Contact us</a>
+          </li>
+        </ul>
+
+      </div>
+
+    </div>
+  </nav>
